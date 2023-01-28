110ea398903495cb308ecea0c1bb4e72f6e32e93
Peter Loftus <loftusp5976@gmail.com>
2023-01-28 16:00:27 -0500

    build: added encryption for enviornment files

---
+++ utils/scripts/env-encrypt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+# npx senv encrypt .env >.env.encrypted
+npx senv encrypt .env.development >.env.development.encrypted
+npx senv encrypt .env.production >.env.production.encrypted
