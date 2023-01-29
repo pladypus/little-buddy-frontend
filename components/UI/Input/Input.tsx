a574dbc4a3447c558455038e79aa2b0f959e80f5
Peter Loftus <loftusp5976@gmail.com>
2023-01-28 19:51:06 -0500

    docs: added jdoc to input component

--- components/UI/Input/Input.tsx
+++ components/UI/Input/Input.tsx
@@ -1,5 +1,10 @@
 import { InputHTMLAttributes } from "react";
 
+/**
+ * Input Component with styling applied
+ * @param props
+ * @returns
+ */
 const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
   return <input {...props} data-testid="input-test" />;
 };
