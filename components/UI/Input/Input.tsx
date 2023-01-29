e2fa6efb43a65ebde85c6ba9e4e85a0c496b73b7
Peter Loftus <loftusp5976@gmail.com>
2023-01-28 19:52:52 -0500

    docs: added return comment to input jdoc

--- components/UI/Input/Input.tsx
+++ components/UI/Input/Input.tsx
@@ -3,7 +3,7 @@ import { InputHTMLAttributes } from "react";
 /**
  * Input Component with styling applied
  * @param props
- * @returns
+ * @returns JSX Elecment
  */
 const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
   return <input {...props} data-testid="input-test" />;
