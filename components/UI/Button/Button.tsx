b53e6c43461c1a7531b5d0b39b70dddc88dd5d21
Peter Loftus <loftusp5976@gmail.com>
2023-01-28 19:56:47 -0500

    docs: added jdoc to Button component

--- components/UI/Button/Button.tsx
+++ components/UI/Button/Button.tsx
@@ -1,5 +1,10 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
+/**
+ * Button Component with styling applied
+ * @param props
+ * @returns JSX Elecment
+ */
 const Button: React.FC<
   ButtonHTMLAttributes<HTMLButtonElement> & { children?: ReactNode }
 > = (props) => {
