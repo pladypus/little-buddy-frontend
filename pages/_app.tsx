f617ced37a4389afba5af40240ee112fd3462659
Peter Loftus <loftusp5976@gmail.com>
2023-02-12 13:16:45 -0500

    feat: added dark mode support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useDarkMode } from "@/hooks";
 import "@/styles/globals.css";
 import configLogger from "@/utils/logger";
 import type { AppProps } from "next/app";
@@ -5,6 +6,8 @@ import type { AppProps } from "next/app";
 configLogger();
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useDarkMode();
+
   return <Component {...pageProps} />;
 };
 
