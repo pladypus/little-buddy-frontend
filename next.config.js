d4472ee9f785feb3adf59ee587705462568e646d
Peter Loftus <loftusp5976@gmail.com>
2023-04-09 14:30:54 -0400

    build(nextjs): updated config to support images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api-ninjas.com",
+        port: "",
+        pathname: "/images/dogs/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
