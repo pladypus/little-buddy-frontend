9d7d6b4a974b2b5930d990cf0c4ba52fe9b6860f
Peter Loftus <loftusp5976@gmail.com>
2023-04-09 14:32:23 -0400

    fix(input): added support for refs

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,12 +1,23 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
 
 /**
  * Input Component with styling applied
  * @param props
  * @returns JSX Elecment
  */
-const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-  return <input {...props} data-testid="input-test" />;
-};
+// eslint-disable-next-line react/display-name
+const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+  return (
+    <input
+      ref={ref}
+      {...props}
+      data-testid="input-test"
+      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${props.className}`}
+    />
+  );
+});
 
 export default Input;
