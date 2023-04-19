7fa7a9665691d5b2d0cd449589e36237c8ca00c9
Peter Loftus <loftusp5976@gmail.com>
2023-04-18 22:59:52 -0400

    fix(input): updated dark mode bg color

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -15,7 +15,7 @@ const Input = forwardRef<
       ref={ref}
       {...props}
       data-testid="input-test"
-      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${props.className}`}
+      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-blue-500 dark:focus:border-blue-500 ${props.className}`}
     />
   );
 });
